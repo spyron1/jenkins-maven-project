923b951859f7fdbb1ae7a54a1c6f8d7661667a66
DIVYANSHU AARYAN <49833227+spyron1@users.noreply.github.com>
2025-07-13 13:38:35 +0530

    Create App.java
    java file

---
+++ App.java
@@ -0,0 +1,8 @@
+// filepath: src\main\java\com\example\App.java
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
